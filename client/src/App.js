969ab35fa8e3034aae98e946b4b9891207ce4585
mehdi-benhariz <benharizmehdi20@gmail.com>
2022-03-31 23:02:36 +0100

    first template

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <div class="bg-teal-400 first-letter:to-blue-600">Hello Tailwind!</div>
+      {/* page 1 */}
+      <div>nav bar + search</div>
+      <div>
+        <h2>Recents used docs </h2>
+        <div>Doc picture with name</div>
+      </div>
+      {/* page 2 */}
+      <div>
+        <div>nav bar + edit choices ...</div>
+        <div>content updated in real time</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
